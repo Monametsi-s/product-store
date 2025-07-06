e771e4e0770de7c6f0f06e0b4fa2353c1a12aae2
MONAMETSI SEELE <Seelem@btc.bw>
2025-07-06 17:16:59 +0200

    finish backend

--- backend/server.js
+++ backend/server.js
@@ -1,73 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-// import Product from "./models/product.model.js";
+import productRoutes from "./routes/product.route.js";
 
+// Importing dotenv to manage environment variables
 dotenv.config();
 
+// Creating an instance of express
 const app = express();
+
+// Middleware to parse incoming JSON requests
+app.use(express.json()); // <-- This line is crucial
 const PORT = process.env.PORT || 5000;
 
-// app.get("api/products" ,async (req, res) => {
-// 	try {
-// 		const products = await Product.find({});
-// 		res.status(200).json({ success: true, data: products });
-// 	} catch (error) {
-// 		console.log("error in fetching products:", error.message);
-// 		res.status(500).json({ success: false, message: "Server Error" });
-// 	}
-// }
+app.use("/api/products", productRoutes);
 
-app.post("/products", async (req, res) => {
-	const product = req.body; // user will send this data
-
-	if (!product.name || !product.price || !product.image) {
-		return res.status(400).json({ success: false, message: "Please provide all fields" });
-	}
-	const newProduct = new Product(product);
-	try {
-		await newProduct.save();
-		res.status(201).json({ success: true, data: newProduct });
-	} catch (error) {
-		console.error("Error in Create product:", error.message);
-		res.status(500).json({ success: false, message: "Server Error" });
-	}
-});
-
-
-
-// export const updateProduct = async (req, res) => {
-// 	const { id } = req.params;
-
-// 	const product = req.body;
-
-// 	if (!mongoose.Types.ObjectId.isValid(id)) {
-// 		return res.status(404).json({ success: false, message: "Invalid Product Id" });
-// 	}
-
-// 	try {
-// 		const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true });
-// 		res.status(200).json({ success: true, data: updatedProduct });
-// 	} catch (error) {
-// 		res.status(500).json({ success: false, message: "Server Error" });
-// 	}
-// };
-
-// export const deleteProduct = async (req, res) => {
-// 	const { id } = req.params;
-
-// 	if (!mongoose.Types.ObjectId.isValid(id)) {
-// 		return res.status(404).json({ success: false, message: "Invalid Product Id" });
-// 	}
-
-// 	try {
-// 		await Product.findByIdAndDelete(id);
-// 		res.status(200).json({ success: true, message: "Product deleted" });
-// 	} catch (error) {
-// 		console.log("error in deleting product:", error.message);
-// 		res.status(500).json({ success: false, message: "Server Error" });
-// 	}
-// };
 
 app.listen(PORT, () => {
 	connectDB();
