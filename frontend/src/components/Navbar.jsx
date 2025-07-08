0ab6d13f98f41f1e5bf0b00dff8c91cec208bdb6
Monametsi <elboncaptain@gmail.com>
2025-07-08 06:06:01 +0200

    Add colorMode feature

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,18 +1,46 @@
-import { Container, Flex } from '@chakra-ui/react'
-import React from 'react'
+import { Button, Container, Flex, HStack, Text} from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import { PlusSquareIcon } from "@chakra-ui/icons";
+import { IoMoon } from 'react-icons/io5';
+import { LuSun } from 'react-icons/lu';
+import { useColorMode } from '@chakra-ui/react';
 
-export default function Navbar() {
+const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <Container maxW="1140px" p={4}>
-      <Flex>
+    <Container maxW={"1140px"} p={4}>
+      <Flex
         h={16}
-        alignItems="center"
-        justifyContent="space-between"
-        {/* flexDir={
-          { base: 'column', md: 'row' }
-        } */}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{
+          base: 'column', 
+          sm: 'row' }}
+      >
+      <Text
+        fontSize={{base: "22", sm: "28" }}
+        fontWeight={"bold"}
+        textTransform={"uppercase"}
+        textAlign={"center"}
+        bgGradient={"linear(to-r, cyan.400, blue.500)"}
+        bgClip={"text"}       
+      >
+        <Link to={"/"}>Product Store🛒</Link>
+      </Text>
+      <HStack spacing={2} alignItems={"center"}>
+        <Link to={"/create"}>
+        <Button>
+          <PlusSquareIcon fontSize={20} />
+        </Button>
+        </Link>
+        <Button onClick={toggleColorMode}>{colorMode === "light" ? <IoMoon /> : 
+        <LuSun size='20' />}
+        </Button>
+      </HStack>
       </Flex>
     </Container>
 
   )
 }
+
+export default Navbar;
