335b82c7708a21694e6372205b1c5aaa5fa6842b
Monametsi <elboncaptain@gmail.com>
2025-07-15 10:13:05 +0200

    Add serve static files in production feature

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const __dirname = path.resolve();
 app.use("/api/products", productRoutes);
 
 // Serve static files from the frontend build directory in production
-if(process.env.NODE_EV === "production") {
+if(process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "frontend/dist")));
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
