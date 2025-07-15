4350aae9dde6a9c0b8da524e9aca33306d76f3ca
Monametsi <elboncaptain@gmail.com>
2025-07-15 09:36:26 +0200

    Add server static file code

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
 
@@ -13,9 +14,17 @@ const app = express();
 app.use(express.json()); // <-- This line is crucial
 const PORT = process.env.PORT || 5000;
 
+
+const __dirname = path.resolve();
 app.use("/api/products", productRoutes);
 
-
+// Serve static files from the frontend build directory in production
+if(process.env.NODE_EV === "production") {
+	app.use(express.static(path.join(__dirname, "frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	})
+}
 app.listen(PORT, () => {
 	connectDB();
 	console.log("Server started at http://localhost:" + PORT);
